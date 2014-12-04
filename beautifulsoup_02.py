cae00dd82e82ee718f434efa65560fa693c30997
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-12-04 11:48:42 -0500

    first commit

--- beautifulsoup_02.py
+++ beautifulsoup_02.py
@@ -5,9 +5,13 @@ URL = 'http://www.whitehouse.gov/net-neutrality'
 DATA = urllib2.urlopen(URL).read()
 HTML_SOUP = BeautifulSoup(DATA)
 
-# Put your function here
+def obama_net_neutrality(HTML_SOUP):
+    speech = HTML_SOUP.findAll('tt')
+    for lines in speech:
+        print lines
+print obama_net_neutrality(HTML_SOUP)
 
-if __name__ == '__main__':
-    # You can use this conditional block for debugging
-    # print HTML_SOUP
-    pass
+# if __name__ == '__main__':
+#     # You can use this conditional block for debugging
+#     print HTML_SOUP
+#     pass
