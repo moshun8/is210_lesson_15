63e2ab6240d6721b6f08fb725d8ec45bece64ee5
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-12-04 11:58:49 -0500

    first paragraph, not whole speech below it

--- beautifulsoup_02.py
+++ beautifulsoup_02.py
@@ -5,11 +5,13 @@ URL = 'http://www.whitehouse.gov/net-neutrality'
 DATA = urllib2.urlopen(URL).read()
 HTML_SOUP = BeautifulSoup(DATA)
 
-def obama_net_neutrality(HTML_SOUP):
-    speech = HTML_SOUP.findAll('tt')
+
+def obama_net_neutrality():
+    '''obama speech docstring'''
+    speech = HTML_SOUP.findAll('p', {'class': 'intro-paragraph'})
     for lines in speech:
         print lines
-print obama_net_neutrality(HTML_SOUP)
+# print obama_net_neutrality()
 
 # if __name__ == '__main__':
 #     # You can use this conditional block for debugging
