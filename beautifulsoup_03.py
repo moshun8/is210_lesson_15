bfe95ba5e0a61cfa6c6da928481055b39ae1f638
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-12-05 14:54:09 -0500

    some of task 3

--- beautifulsoup_03.py
+++ beautifulsoup_03.py
@@ -8,19 +8,21 @@ SOUP = BeautifulSoup(DATA)
 def sps_it_department():
     '''finds names and emails and puts in dict'''
     card_list = []
-    cards = SOUP.find_all('div', {'class': 'card unit size1of2'})
-    names = cards.find_all('span', {'class': 'name-bold'})
-    email = cards.find_all('a', {'href': 'mailto:'})
+
+    cards = SOUP.find_all('div', {'class': 'card unit size1of2'})   
+    # cards = SOUP.find('div', {'class':'card unit size1of2'})
+
+    names = SOUP.find_all('span', {'class': 'name-bold'})
+
+    mailtos = SOUP.select('a[href^=mailto]')
+
     name_dict = {}
-    first = names[0]
-    print first
-    last = names[1]
-    print last
+
+    for entry in cards:
+        card_dict = {}
+
     # name_dict[staff] = (first, last, email)
 
-print sps_it_department
 
 if __name__ == '__main__':
-    # You can use this conditional block for debugging
-    # print SOUP
-    pass
+    sps_it_department()
