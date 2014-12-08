0b7a2235f481a68c92bc146979a8d2555c4642eb
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-12-07 20:53:37 -0500

    task 3

--- beautifulsoup_03.py
+++ beautifulsoup_03.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''Week 15 Task 3'''
+
 import urllib2
 from bs4 import BeautifulSoup
 
@@ -5,23 +9,25 @@ URL = 'http://sps.cuny.edu/whoweare/departments/it'
 DATA = urllib2.urlopen(URL).read()
 SOUP = BeautifulSoup(DATA)
 
+
 def sps_it_department():
     '''finds names and emails and puts in dict'''
     card_list = []
 
-    cards = SOUP.find_all('div', {'class': 'card unit size1of2'})   
+    cards = SOUP.find_all('div', {'class': 'card unit size1of2'})
     # cards = SOUP.find('div', {'class':'card unit size1of2'})
+    for person in cards:
+        names = person.find_all('span', {'class': 'name-bold'})
+        mailtos = person.select('a[href^=mailto]')
 
-    names = SOUP.find_all('span', {'class': 'name-bold'})
-
-    mailtos = SOUP.select('a[href^=mailto]')
-
-    name_dict = {}
-
-    for entry in cards:
-        card_dict = {}
-
-    # name_dict[staff] = (first, last, email)
+        for partof, entry in enumerate(names):
+            last_first = entry.split(',').strip()
+            card_dict = {
+                'email': partof.contents[0],
+                'first_name': last_first[1],
+                'last_name': last_first[0]
+            }
+        return card_list.append(card_dict)
 
 
 if __name__ == '__main__':
